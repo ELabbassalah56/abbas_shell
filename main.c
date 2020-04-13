22b5f10f8a482074d4f48ce837d149bedac4c55c
Osama Adam <osamaadam98@gmail.com>
2020-04-13 02:44:25 +0200

    fixed background tasks

--- main.c
+++ main.c
@@ -1,50 +1,40 @@
 
 #include "shell.h"
 
+int main(void) {
+  char inputString[MAXCOM];        // buffer to hold command entered
+  char *parsedArgs[MAXLIST];       // command line
+  char *parsedArgsPiped[MAXLIST];  // piped args
+  int execFlag = 0;                // execute flag
 
-int main(void) 
-{ 
-    char inputString[MAXCOM];//buffer to hold command entered  
-    char *parsedArgs[MAXLIST];//command line 
-    char *parsedArgsPiped[MAXLIST];//piped args 
-    int execFlag = 0; // execute flag 
-  
-  
-    //init my shell
-    init_shell();
-    //show my shell
-    printf(ANSI_COLOR_GREEN   "*************************************************"ANSI_COLOR_RESET "\n");
-	printf(ANSI_COLOR_RESET  "\t\t***EL_ABBAS_SHELL***"   ANSI_COLOR_RESET "\n");
-  printf(ANSI_COLOR_GREEN   "*************************************************"ANSI_COLOR_RESET "\n");
-    //dynamic code superloop
-    while(True){
-     
-      
-      //print EX directory
-       printEXDir();
-      // take input 
-      if (takeInput(inputString)) 
-          continue;
-          // process 
-        execFlag = process_Line(inputString, 
-        parsedArgs, parsedArgsPiped); 
-        // execflag returns zero if there is no command 
-        // or it is a builtin command, 
-        // 1 if it is a simple command 
-        // 2 if it is including a pipe.
-        
-           // execute 
-        if (execFlag == 1) 
-            exec_Args(parsedArgs); 
-  
-        if (execFlag == 2) 
-            execArgsPiped(parsedArgs, parsedArgsPiped); 
-      
-    
-    
-    } 
-    return 0; 
-} 
+  // init my shell
+  // init_shell();
+  // // show my shell
+  printf(ANSI_COLOR_GREEN
+         "*************************************************" ANSI_COLOR_RESET
+         "\n");
+  printf(ANSI_COLOR_RESET "\t\t***EL_ABBAS_SHELL***" ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_GREEN
+         "*************************************************" ANSI_COLOR_RESET
+         "\n");
+  // dynamic code superloop
+  while (True) {
+    // print EX directory
+    printEXDir();
+    // take input
+    if (takeInput(inputString)) continue;
+    // process
+    int size;
+    execFlag = process_Line(inputString, parsedArgs, parsedArgsPiped, &size);
+    // execflag returns zero if there is no command
+    // or it is a builtin command,
+    // 1 if it is a simple command
+    // 2 if it is including a pipe.
 
-      
- 
+    // execute
+    if (execFlag == 1) exec_Args(parsedArgs, size);
+
+    if (execFlag == 2) execArgsPiped(parsedArgs, parsedArgsPiped);
+  }
+  return 0;
+}
--- shell.h
+++ shell.h
@@ -1,100 +1,101 @@
-#include <dirent.h> 
-#include<stdio.h> 
-#include<string.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<sys/wait.h> 
+#include <dirent.h>
 #include <fcntl.h>
-#include <termios.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <termios.h>
 #include <time.h>
+#include <unistd.h>
 
 /*Use these colors to print colored text on the console*/
 
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 /********************defines*********************/
-//#define SIGHUP  1   /* Hangup the process */ 
-//#define SIGINT  2   /* Interrupt the process */ 
-//#define SIGQUIT 3   /* Quit the process */ 
-//#define SIGILL  4   /* Illegal instruction. */ 
-//#define SIGTRAP 5   /* Trace trap. */ 
+//#define SIGHUP  1   /* Hangup the process */
+//#define SIGINT  2   /* Interrupt the process */
+//#define SIGQUIT 3   /* Quit the process */
+//#define SIGILL  4   /* Illegal instruction. */
+//#define SIGTRAP 5   /* Trace trap. */
 //#define SIGABRT 6   /* Abort. */
-#define  True 1
-#define  False 0
-#define MAXCOM 1000 // max number of letters to be supported 
-#define MAXLIST 100 // max number of commands to be supported
+#define True 1
+#define False 0
+#define MAXCOM 1000  // max number of letters to be supported
+#define MAXLIST 100  // max number of commands to be supported
 // Clearing the shell using escape sequences
 //\033 - ASCII escape character
 //[H - move the cursor to the home position
-//[J - erases the screen from the current line down to the bottom of the screen  
+//[J - erases the screen from the current line down to the bottom of the screen
 #define clc() printf("\033[H\033[J")
-// Help command builtin 
-#define openHelp() \
-    puts(ANSI_COLOR_GREEN "\n***WELCOME TO EL_ABBAS SHELL HELP***"\
-        "\nCopyright @ Suprotik Dey"\
-        "\n-Use the shell at your own risk..."\
-        "\nList of Commands supported:"\
-        "\n>cd"\
-        "\n>ls"\
-        "\n>exit"\
-        "\n>all other general commands available in UNIX shell"\
-        "\n>pipe handling"\
-        "\n>improper space handling")
+// Help command builtin
+#define openHelp()                                             \
+  puts(ANSI_COLOR_GREEN                                        \
+       "\n***WELCOME TO EL_ABBAS SHELL HELP***"                \
+       "\nCopyright @ Suprotik Dey"                            \
+       "\n-Use the shell at your own risk..."                  \
+       "\nList of Commands supported:"                         \
+       "\n>cd"                                                 \
+       "\n>ls"                                                 \
+       "\n>exit"                                               \
+       "\n>all other general commands available in UNIX shell" \
+       "\n>pipe handling"                                      \
+       "\n>improper space handling")
 
-
+void terminator(pid_t sig_Pid);
 /*******************function***********************/
-// Greeting shell during startup 
-void init_shell() 
-{ 
-    clc(); 
-    printf(ANSI_COLOR_BLUE"\n\n\n\n******************"
-        "************************"); 
-    printf( ANSI_COLOR_GREEN"\n\n\n\t****EL_ABBAS SHELL****"); 
-    printf(ANSI_COLOR_BLUE"\n\n\t-NAGA7ANA YA BASHMOHANDES BELLAH 3ALIK-"); 
-    printf(ANSI_COLOR_BLUE"\n\n\n\n*******************"
-               "***********************"); 
-    char* username = getenv("USER"); 
-    printf(ANSI_COLOR_BLUE"\n\n\n>>USER is: @%s", username); 
-    printf("\n"); 
-    sleep(2); 
-    clc();
-} 
-//printing internal directory  directory 
-int  printINDir(void) {
+// Greeting shell during startup
+void init_shell() {
+  clc();
+  printf(ANSI_COLOR_BLUE
+         "\n\n\n\n******************"
+         "************************");
+  printf(ANSI_COLOR_GREEN "\n\n\n\t****EL_ABBAS SHELL****");
+  printf(ANSI_COLOR_BLUE "\n\n\t-NAGA7ANA YA BASHMOHANDES BELLAH 3ALIK-");
+  printf(ANSI_COLOR_BLUE
+         "\n\n\n\n*******************"
+         "***********************");
+  char* username = getenv("USER");
+  printf(ANSI_COLOR_BLUE "\n\n\n>>USER is: @%s", username);
+  printf("\n");
+  sleep(2);
+  clc();
+}
+// printing internal directory  directory
+int printINDir(void) {
+  struct dirent* de;  // Pointer for directory entry
 
-struct dirent *de;  // Pointer for directory entry 
-  
-    // opendir() returns a pointer of DIR type.  
-    DIR *dr = opendir("."); 
-  
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
-    { 
-        printf(ANSI_COLOR_RED "Could not open current directory" ); 
-        return 0; 
-    } 
-   
-    // read DIR using  readdir() 
-    while ((de = readdir(dr)) != NULL) 
-            printf("%s ", de->d_name); 
-  
-    closedir(dr); 
-     }
-//printing external directory 
- int printEXDir(void) { 
-    char cwd[MAXCOM];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf(ANSI_COLOR_RED "mark@"ANSI_COLOR_RESET"EL_ABBAS-"ANSI_COLOR_GREEN"%s"ANSI_COLOR_RED":~$", cwd);
-   } else {
-       perror("getcwd() error");
-       return 1;
-   }
-} 
+  // opendir() returns a pointer of DIR type.
+  DIR* dr = opendir(".");
+
+  if (dr == NULL)  // opendir returns NULL if couldn't open directory
+  {
+    printf(ANSI_COLOR_RED "Could not open current directory");
+    return 0;
+  }
+
+  // read DIR using  readdir()
+  while ((de = readdir(dr)) != NULL) printf("%s ", de->d_name);
+
+  closedir(dr);
+}
+// printing external directory
+int printEXDir(void) {
+  char cwd[MAXCOM];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf(ANSI_COLOR_RED "mark@" ANSI_COLOR_RESET "EL_ABBAS-" ANSI_COLOR_GREEN
+                          "%s" ANSI_COLOR_RED ":~$",
+           cwd);
+  } else {
+    perror("getcwd() error");
+    return 1;
+  }
+}
 // remove end of line
 void remove_end_of_line(char line[MAXCOM]) {
   int index = 0;
@@ -103,158 +104,149 @@ void remove_end_of_line(char line[MAXCOM]) {
   }
   line[index] = '\0';
 }
-//function to readline 
-char * readline(char str[]){
- printf(":- "ANSI_COLOR_RESET);
-  char *ret = fgets(str, MAXCOM, stdin);
+// function to readline
+char* readline(char str[]) {
+  printf(":- " ANSI_COLOR_RESET);
+  char* ret = fgets(str, MAXCOM, stdin);
   remove_end_of_line(ret);
 
-  return ret ;
+  return ret;
 }
 
-// function for parsing command words 
-void parseSpace(char* str, char** parsed) 
-{ 
-     
-  
-    for (int index = 0; index < MAXLIST; index++) { 
-        parsed[index] = strsep(&str, "  "); 
-  
-        if (parsed[index] == NULL) 
-            break; 
-        if (strlen(parsed[index]) == 0) 
-            index--; 
-    } 
-} 
- 
+// function for parsing command words
+int parseSpace(char* str, char** parsed) {
+  int index = 0;
+  for (index = 0; index < MAXLIST; index++) {
+    parsed[index] = strsep(&str, "  ");
 
-
-
-
-// Function to take input 
-int takeInput(char* str) 
-{ 
-    char* temp; 
-  
-    temp = readline(str); 
-    if (strlen(temp) != 0) { 
-       // add_history(temp); 
-        strcpy(str, temp); 
-        return 0; 
-    } 
-    else { 
-        return 1; 
-    } 
+    if (parsed[index] == NULL) break;
+    if (strlen(parsed[index]) == 0) index--;
+  }
+  return index;
 }
-//log function
-void log_F(char * parsed){
 
-   if (parsed == "log") {
-          const char* log[] = {"vi", "shell.log", NULL};
-          execvp(log[0], (char**)log);
-        } else
-          execvp(parsed[0], parsed);
-      }
+// Function to take input
+int takeInput(char* str) {
+  char* temp;
 
-// Function where the system command is executed 
-void exec_Args(char** parsed) 
-{ 
-    // Forking a child 
-    pid_t pid = fork();  
-  
-    if (pid == -1) { 
-        printf("\nFailed forking child.."); 
-        return; 
-    } else if (pid == 0) { 
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\nCould not execute command..\n"); 
-        } exit(0);
-        
-      if ((strcmp(parsed,"&")==0 )|| strcmp(parsed,"cd")==0  ) {
+  temp = readline(str);
+  if (strlen(temp) != 0) {
+    // add_history(temp);
+    strcpy(str, temp);
+    return 0;
+  } else {
+    return 1;
+  }
+}
+// log function
+void log_F(char* parsed) {
+  if (parsed == "log") {
+    const char* log[] = {"vi", "shell.log", NULL};
+    execvp(log[0], (char**)log);
+  } else
+    execvp(parsed[0], parsed);
+}
+
+// Function where the system command is executed
+void exec_Args(char** parsed, int size) {
+  // Forking a child
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    printf("\nFailed forking child..");
+    return;
+  } else if (pid == 0) {
+    if (size > 0 && !strcmp(parsed[size - 1], "&")) {
+      printf("I'm here\n");
+      exit(0);
+    } else {
+      execvp(parsed[0], parsed);
+      exit(0);
+    }
+  } else {
+    // waiting for child to terminate
+
+    if (size > 0 && !strcmp(parsed[size - 1], "&")) {
+      parsed[size - 1] = NULL;
+      exec_Args(parsed, 0);
+      signal(SIGCHLD, terminator);
+    } else {
+      wait(NULL);
+    }
+  }
+}
+// Function where the piped system commands is executed
+void execArgsPiped(char** parsed, char** parsedpipe) {
+  // 0 is read end, 1 is write end
+  int pipefd[2];
+  pid_t p1, p2;
+
+  if (pipe(pipefd) < 0) {
+    printf("\nPipe could not be initialized");
+    return;
+  }
+  p1 = fork();
+  if (p1 < 0) {
+    printf("\nCould not fork");
+    return;
+  }
+
+  if (p1 == 0) {
+    // Child 1 executing..
+    // It only needs to write at the write end
+    close(pipefd[0]);
+    dup2(pipefd[1], STDOUT_FILENO);
+    close(pipefd[1]);
+
+    if (execvp(parsed[0], parsed) < 0) {
+      printf("\nCould not execute command 1..");
+      exit(0);
+    }
+  } else {
+    // Parent executing
+    p2 = fork();
+
+    if (p2 < 0) {
+      printf("\nCould not fork");
+      return;
+    }
+
+    // Child 2 executing..
+    // It only needs to read at the read end
+    if (p2 == 0) {
+      close(pipefd[1]);
+      dup2(pipefd[0], STDIN_FILENO);
+      close(pipefd[0]);
+      if (execvp(parsedpipe[0], parsedpipe) < 0) {
+        printf("\nCould not execute command 2..");
         exit(0);
       }
-      else {
-      log_F(parsed);
-      }}
-     else { 
-        // waiting for child to terminate 
-        wait(NULL);  
-        return; 
-    } 
+    } else {
+      // parent executing, waiting for two children
+      wait(NULL);
+      wait(NULL);
+    }
+  }
 }
-// Function where the piped system commands is executed 
-void execArgsPiped(char** parsed, char** parsedpipe) 
-{ 
-    // 0 is read end, 1 is write end 
-    int pipefd[2];  
-    pid_t p1, p2; 
-  
-    if (pipe(pipefd) < 0) { 
-        printf("\nPipe could not be initialized"); 
-        return; 
-    } 
-    p1 = fork(); 
-    if (p1 < 0) { 
-        printf("\nCould not fork"); 
-        return; 
-    } 
-  
-    if (p1 == 0) { 
-        // Child 1 executing.. 
-        // It only needs to write at the write end 
-        close(pipefd[0]); 
-        dup2(pipefd[1], STDOUT_FILENO); 
-        close(pipefd[1]); 
-  
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\nCould not execute command 1.."); 
-            exit(0); 
-        } 
-    } else { 
-        // Parent executing 
-        p2 = fork(); 
-  
-        if (p2 < 0) { 
-            printf("\nCould not fork"); 
-            return; 
-        } 
-  
-        // Child 2 executing.. 
-        // It only needs to read at the read end 
-        if (p2 == 0) { 
-            close(pipefd[1]); 
-            dup2(pipefd[0], STDIN_FILENO); 
-            close(pipefd[0]); 
-            if (execvp(parsedpipe[0], parsedpipe) < 0) { 
-                printf("\nCould not execute command 2.."); 
-                exit(0); 
-            } 
-        } else { 
-            // parent executing, waiting for two children 
-            wait(NULL); 
-            wait(NULL); 
-        } 
-    } 
-} 
-  //terminator function
+// terminator function
 void terminator(pid_t sig_Pid) {
   int status;
   pid_t processPid;
   time_t localTime = time(NULL);
-  FILE *logFile;
+  FILE* logFile;
 
   switch (sig_Pid) {
     case SIGCHLD:
       logFile = fopen("./shell.log", "a+");
       while ((processPid = waitpid(-1, &status, WNOHANG)) > 0) {
-        fprintf(logFile,"%s\tProcess %d exited with status %d\n",
+        fprintf(logFile, "%s\tProcess %d exited with status %d\n",
                 asctime(localtime(&localTime)), processPid, status);
       }
       fclose(logFile);
       break;
 
     case SIGQUIT:
-      printf("\n***Goodbye***\n"); 
+      printf("\n***Goodbye***\n");
       exit(0);
       break;
 
@@ -264,102 +256,83 @@ void terminator(pid_t sig_Pid) {
   }
 }
 
+// Function to execute builtin commands
+int ownCmdHandler(char** parsed) {
+  int NoOfOwnCmds = 4, switchOwnArg = 0;
+  char* ListOfOwnCmds[NoOfOwnCmds];
+  char* username;
 
+  ListOfOwnCmds[0] = "exit";
+  ListOfOwnCmds[1] = "cd";
+  ListOfOwnCmds[2] = "help";
+  ListOfOwnCmds[3] = "hello";
+  ListOfOwnCmds[4] = "log";
 
-  
-// Function to execute builtin commands 
-int ownCmdHandler(char** parsed) 
-{ 
-    int NoOfOwnCmds = 4, switchOwnArg = 0; 
-    char* ListOfOwnCmds[NoOfOwnCmds]; 
-    char* username; 
-  
-    ListOfOwnCmds[0] = "exit"; 
-    ListOfOwnCmds[1] = "cd"; 
-    ListOfOwnCmds[2] = "help"; 
-    ListOfOwnCmds[3] = "hello";
-    ListOfOwnCmds[4] = "log";
-  
-    for (int index = 0; index < NoOfOwnCmds; index++) { 
-        if (strcmp(parsed[0], ListOfOwnCmds[index]) == 0) { 
-            switchOwnArg = index + 1; 
-            break; 
-        } 
-    } 
-  
-    switch (switchOwnArg) { 
-    case 1: 
-        terminator(SIGQUIT);
-    case 2: 
-        chdir(parsed[1]); 
-        return 1; 
-    case 3: 
-        openHelp(); 
-        return 1; 
-    case 4: 
-        username = getenv("USER"); 
-        printf("\nHello %s.\nMind that this is "
-            "not a place to play around."
-            "\nUse help to know more..\n", 
-            username); 
-        return 1;
+  for (int index = 0; index < NoOfOwnCmds; index++) {
+    if (strcmp(parsed[0], ListOfOwnCmds[index]) == 0) {
+      switchOwnArg = index + 1;
+      break;
+    }
+  }
+
+  switch (switchOwnArg) {
+    case 1:
+      terminator(SIGQUIT);
+    case 2:
+      chdir(parsed[1]);
+      return 1;
+    case 3:
+      openHelp();
+      return 1;
+    case 4:
+      username = getenv("USER");
+      printf(
+          "\nHello %s.\nMind that this is "
+          "not a place to play around."
+          "\nUse help to know more..\n",
+          username);
+      return 1;
     case 5:
-        log_F(parsed);
-       
-    default: 
-    
-        break; 
-    } 
-  
-    return 0; 
+      log_F(parsed);
+
+    default:
+
+      break;
+  }
+
+  return 0;
 }
-// function for finding pipe 
-int parsePipe(char* str, char** str_piped) 
-{ 
-    int i; 
-    for (i = 0; i < 2; i++) { 
-        str_piped[i] = strsep(&str, "|"); 
-        if (str_piped[i] == NULL) 
-            break; 
-    } 
-  
-    if (str_piped[1] == NULL) 
-        return 0; // returns zero if no pipe is found. 
-    else { 
-        return 1; 
-    } 
+// function for finding pipe
+int parsePipe(char* str, char** str_piped) {
+  int i;
+  for (i = 0; i < 2; i++) {
+    str_piped[i] = strsep(&str, "|");
+    if (str_piped[i] == NULL) break;
+  }
+
+  if (str_piped[1] == NULL)
+    return 0;  // returns zero if no pipe is found.
+  else {
+    return 1;
+  }
 }
-//process on line 
-int process_Line(char* str, char** parsed, char** parsedpipe) 
-{ 
-  
-    char* strpiped[3]; 
-    int piped = 0; 
-  
-    piped = parsePipe(str, strpiped); 
-  
-    if (piped) { 
-        parseSpace(strpiped[0], parsed); 
-        parseSpace(strpiped[1], parsedpipe); 
-         
-    } else { 
-  
-        parseSpace(str, parsed); 
-    
-    } 
-  
-    if (ownCmdHandler(parsed)) 
-        return 0; 
-    else
-        return 1 + piped; 
-} 
+// process on line
+int process_Line(char* str, char** parsed, char** parsedpipe, int* size) {
+  char* strpiped[3];
+  int piped = 0;
 
+  piped = parsePipe(str, strpiped);
 
+  if (piped) {
+    parseSpace(strpiped[0], parsed);
+    parseSpace(strpiped[1], parsedpipe);
 
+  } else {
+    *size = parseSpace(str, parsed);
+  }
 
-
-
-
-
-
-
+  if (ownCmdHandler(parsed))
+    return 0;
+  else
+    return 1 + piped;
+}
